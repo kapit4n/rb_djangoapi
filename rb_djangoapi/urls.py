5aa0be69f3f8a058c036b64f4814157059999fdc
lartl <luis.arce22@gmail.com>
2018-06-30 21:51:44 -0400

    Rest resource for users model

--- rb_djangoapi/urls.py
+++ rb_djangoapi/urls.py
@@ -15,8 +15,28 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from django.contrib.auth.models import User
+from rest_framework import serializers, viewsets, routers
+from django.db import models
+from django.template.defaultfilters import slugify
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide a way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
-
 ]
